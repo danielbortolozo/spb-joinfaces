2fd5295e0cf6dc70a7470719fa014c635bf768a3
Daniel Bortolozo <danielbortolozo@hotmail.com>
2019-10-27 20:08:44 -0300

    First commit

---
+++ src/main/java/com/codenotfound/primefaces/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.codenotfound.primefaces.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+  private String senha;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // require all requests to be authenticated except for the resources
+    http.authorizeRequests().antMatchers("/javax.faces.resource/**")
+        .permitAll().anyRequest().authenticated();
+    // login
+    http.formLogin().loginPage("/login.xhtml").permitAll()
+        .failureUrl("/login.xhtml?error=true");
+    // logout
+    http.logout().logoutSuccessUrl("/login.xhtml");
+    // not needed as JSF 2.2 is implicitly protected against CSRF
+    http.csrf().disable();
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth)
+      throws Exception {
+    this.senha = "1234";
+    System.out.println("senha ="+this.senha);
+    auth.inMemoryAuthentication().withUser("john.doe")
+        .password("{noop}1234").roles("USER").and()
+        .withUser("del").password("{noop}123").roles("ADMIN");
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+}
